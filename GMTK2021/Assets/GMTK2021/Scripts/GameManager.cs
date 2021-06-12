eeed40c671db427ab7a5b7375e941debc666d3c7
Nougatos <moitheos@gmail.com>
2021-06-12 17:37:27 +0200

    add UI

---
+++ GMTK2021/Assets/GMTK2021/Scripts/GameManager.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    [Tooltip("Time of a game in seconds")]
+    public float GameDuration;
+    public List<TMP_Text> Timer;
+
+    private float actualElapsed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        actualElapsed = GameDuration;
+        //timers = new List<TextMeshPro>();
+        //foreach(GameObject obj in Timer)
+        //{
+        //    timers.Add(obj.GetComponent<TextMeshPro>());
+        //}
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        string actualTime = Mathf.FloorToInt(actualElapsed / 60).ToString("00") + ":" + Mathf.Abs(Mathf.FloorToInt(actualElapsed % 60)).ToString("00");
+        foreach (TMP_Text txt in Timer)
+        {
+            txt.text = actualTime;
+        }
+        if(actualElapsed < 0f)
+        {
+            Debug.Log("Game finished");
+        }
+        actualElapsed -= Time.deltaTime;
+    }
+}
