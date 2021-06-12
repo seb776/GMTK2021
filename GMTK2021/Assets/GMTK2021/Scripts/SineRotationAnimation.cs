a4ed4a66fb79e1111230ec13625c9d58473196f9
seb776 <zagseb776@gmail.com>
2021-06-12 12:15:57 +0200

    Done grass prefabs

--- GMTK2021/Assets/GMTK2021/Scripts/SineRotationAnimation.cs
+++ GMTK2021/Assets/GMTK2021/Scripts/SineRotationAnimation.cs
@@ -4,16 +4,41 @@ using UnityEngine;
 
 public class SineRotationAnimation : MonoBehaviour
 {
-    private static float SPEED = 2.0f;
+    public float Speed = 2.0f;
     public float Angle;
     public float AngleOffset;
     public float TimeOffset;
     public Vector3 AxisRotation;
 
+    public bool IsGrassAnim;
+
+    public MeshRenderer GrassRenderer;
+
+    private void Start()
+    {
+        //if (IsGrassAnim)
+        //{
+        //    const float _minInclusive = 0.9f;
+        //    float r = Random.Range(_minInclusive, 1.0f);
+        //    float g = Random.Range(_minInclusive, 1.0f);
+        //    float b = Random.Range(_minInclusive, 1.0f);
+        //    GrassRenderer.material.color = new Color(r, g, b, 1.0f);
+        //}
+    }
+
 
     void Update()
     {
-        float f = Mathf.Sin(SPEED * Time.realtimeSinceStartup+ TimeOffset) * 0.5f + 0.5f;
+        float f = Mathf.Sin(Speed * Time.realtimeSinceStartup+ TimeOffset) * 0.5f + 0.5f;
+        if (IsGrassAnim)
+        {
+            f = ((Mathf.Sin(Speed * Time.realtimeSinceStartup + TimeOffset + this.transform.position.x*0.5f + this.transform.position.z) *0.25f
+                +Mathf.Sin(Speed*Time.realtimeSinceStartup*0.5f+TimeOffset + this.transform.position.x + this.transform.position.z*0.5f) *0.5f
+                + Mathf.Sin(Speed * Time.realtimeSinceStartup * 0.35f + TimeOffset+this.transform.position.x + this.transform.position.z)
+                ) /3.0f)
+                
+                * 0.5f + 0.5f;
+        }
         this.transform.localRotation = Quaternion.AngleAxis(AngleOffset+Mathf.LerpAngle(-Angle, Angle, f), AxisRotation.normalized);
     }
 }
