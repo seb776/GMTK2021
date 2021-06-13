3f73b3a7b0fe7125f0f98700fc1b9ea8c648e846
Nougatos <moitheos@gmail.com>
2021-06-13 16:26:55 +0200

    Fix

--- GMTK2021/Assets/GMTK2021/Scripts/AntFarm.cs
+++ GMTK2021/Assets/GMTK2021/Scripts/AntFarm.cs
@@ -44,29 +44,22 @@ public class AntFarm : MonoBehaviour
                 return null;
         }
 
-<<<<<<< HEAD
-        GameObject gameObject1 = Instantiate(prefab);
-        var splineCmp = gameObject1.GetComponent<SplineWalker>();
-        splineCmp.spline = DefaultPathObject.GetComponent< BezierSpline>() as BezierSpline;
-        if(EAntType.Scout == type)
-        {
-            AppSingleton.Instance.Scout = gameObject1;
-        }
-        return gameObject1.GetComponent<Ant>();
-=======
         if(prefab != null)
         {
             GameObject gameObject1 = Instantiate(prefab);
             var splineCmp = gameObject1.GetComponent<SplineWalker>();
             gameObject1.tag = tag;
             splineCmp.spline = DefaultPathObject.GetComponent<BezierSpline>();
+            if(EAntType.Scout == type)
+        {
+            AppSingleton.Instance.Scout = gameObject1;
+        }
             return gameObject1.GetComponent<Ant>();
         }
         else
         {
             return null;
         }
->>>>>>> 294b5b2641a452bad458fab1ccf3204bfa33aee6
     }
 }
 
--- GMTK2021/Assets/GMTK2021/Scripts/AppSingleton.cs
+++ GMTK2021/Assets/GMTK2021/Scripts/AppSingleton.cs
@@ -8,20 +8,13 @@ public class AppSingleton : Singleton<AppSingleton>
     public float TimerSpawn;
     public bool spawnUnavailable;
 
-<<<<<<< HEAD
-    public float TimeRemain { get; set; }
-    public float Score { get; set; }
-    public GameObject Scout { get; set; }
-=======
     public Texture2D cursorTexture;
     public Texture2D cursorAttackTexture;
     public CursorMode cursorMode = CursorMode.Auto;
     public Vector2 hotSpot = Vector2.zero;
-
-    // Start is called before the first frame update
->>>>>>> 294b5b2641a452bad458fab1ccf3204bfa33aee6
-
-    // Start is called before the first frame update
+    public float TimeRemain { get; set; }
+    public float Score { get; set; }
+    public GameObject Scout { get; set; }
     void Start()
     {
         ResetGameParameters();
@@ -31,12 +24,9 @@ public class AppSingleton : Singleton<AppSingleton>
     {
         spawnUnavailable = false;
         TimerSpawn = 0f;
-<<<<<<< HEAD
         Score = 0f;
         TimeRemain = 0f;
-=======
         ResetCursor();
->>>>>>> 294b5b2641a452bad458fab1ccf3204bfa33aee6
     }
 
     public void ResetCursor()
