56d8ce9c7eb38098583d577c05dd8dbfdf811028
Stanislas Dolcini <stanislas.dolcini@devinci.fr>
2021-06-13 18:22:09 +0200

    Score board

---
+++ GMTK2021/Assets/ScorePanel.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class ScorePanel : MonoBehaviour
+{
+    public TMP_Text TextData { get; private set; }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        TextData = GetComponent<TMP_Text>();
+        if (!TextData)
+            return;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        TextData.text = "Score:" + AppSingleton.Instance.Score;
+        PlayerPrefs.SetFloat("Score", AppSingleton.Instance.Score);
+    }
+}
