a8cf809a16b48f594ffc02e36303ca99bc40d1a6
Anthony FROISSANT <a.froissant@groupeonepoint.com>
2021-06-13 04:10:55 +0200

    Updated Bezier editor and splinewalker behavior

--- GMTK2021/Assets/GMTK2021/Scripts/BezierCurves/SplineDecorator.cs
+++ GMTK2021/Assets/GMTK2021/Scripts/BezierCurves/SplineDecorator.cs
@@ -3,42 +3,45 @@ using UnityEngine;
 public class SplineDecorator : MonoBehaviour
 {
 
-	public BezierSpline spline;
+    public BezierSpline spline;
 
-	public int frequency;
+    public int frequency;
 
-	public bool lookForward;
+    public bool lookForward;
 
-	public Transform[] items;
+    public Transform[] items;
 
-	private void Awake()
-	{
-		if (frequency <= 0 || items == null || items.Length == 0)
-		{
-			return;
-		}
-		float stepSize = frequency * items.Length;
-		if (spline.Loop || stepSize == 1)
-		{
-			stepSize = 1f / stepSize;
-		}
-		else
-		{
-			stepSize = 1f / (stepSize - 1);
-		}
-		for (int p = 0, f = 0; f < frequency; f++)
-		{
-			for (int i = 0; i < items.Length; i++, p++)
-			{
-				Transform item = Instantiate(items[i]) as Transform;
-				Vector3 position = spline.GetPoint(spline.FindPositionFromLookupTable(p * stepSize * spline.TotalLength));
-				item.transform.localPosition = position;
-				if (lookForward)
-				{
-					item.transform.LookAt(position + spline.GetDirection(p * stepSize));
-				}
-				item.transform.parent = transform;
-			}
-		}
-	}
+    private void Awake()
+    {
+        if (spline != null)
+        {
+            if (frequency <= 0 || items == null || items.Length == 0)
+            {
+                return;
+            }
+            float stepSize = frequency * items.Length;
+            if (spline.Loop || stepSize == 1)
+            {
+                stepSize = 1f / stepSize;
+            }
+            else
+            {
+                stepSize = 1f / (stepSize - 1);
+            }
+            for (int p = 0, f = 0; f < frequency; f++)
+            {
+                for (int i = 0; i < items.Length; i++, p++)
+                {
+                    Transform item = Instantiate(items[i]) as Transform;
+                    Vector3 position = spline.GetPoint(spline.FindPositionFromLookupTable(p * stepSize * spline.TotalLength));
+                    item.transform.localPosition = position;
+                    if (lookForward)
+                    {
+                        item.transform.LookAt(position + spline.GetDirection(p * stepSize));
+                    }
+                    item.transform.parent = transform;
+                }
+            }
+        }
+    }
 }
