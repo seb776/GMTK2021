4213432648ccc61c83431d2702969b074a756685
Théo FERNANDEZ <t.fernandez@groupeonepoint.com>
2021-06-14 20:39:36 +0200

    change spawning zone and fix it with time

--- GMTK2021/Assets/FoodSpawner.cs
+++ GMTK2021/Assets/FoodSpawner.cs
@@ -5,21 +5,36 @@ using UnityEngine;
 public class FoodSpawner : MonoBehaviour
 {
     public List<GameObject> Food;
-    public float SpawnChance = 0.0004f;
-    
+    [Tooltip("Maximum time for spawning new food unit, in second")]
+    public float MaxTimeSpawn = 20f;
+    [Tooltip("Minimum time for spawning new food unit, in second")]
+    public float MinTimeSpawn = 10f;
+    [Tooltip("Radius of spawning zone")]
+    public Vector2 radius;
+
+    private float elapsedTime;
+    private float spawnAt;
     // Start is called before the first frame update
     void Start()
     {
-        
+        elapsedTime = 0f;
+        spawnAt = 0f;
+        if(MaxTimeSpawn < MinTimeSpawn)
+        {
+            Debug.LogError("FoodSpawner : Max Time Spawn < Min Time Spawn ! fix it by changing value !");
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Random.Range(0f, 1f) < SpawnChance)
+        elapsedTime += Time.deltaTime;
+        if (spawnAt < elapsedTime)
         {
-            Vector2 position = Random.insideUnitCircle * 2;
-            Instantiate(Food[Random.Range(0, Food.Count)], new Vector3(transform.position.x - position.x , 0.1f, transform.position.z - position.y), Quaternion.identity);
+            Vector2 position = Random.insideUnitCircle * radius;
+            Instantiate(Food[Random.Range(0, Food.Count)], new Vector3(transform.position.x - position.x , transform.position.y, transform.position.z - position.y), Quaternion.identity);
+            spawnAt = Random.Range(MinTimeSpawn, MaxTimeSpawn);
+            elapsedTime = 0f;
         }
     }
 }
